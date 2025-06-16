31a9a842e9beafb91381058e7ab5017179996787
alberto.lopes <cast.aberto@pe.sebrae.com.br>
2025-06-16 17:34:50 -0300

    alteração home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -148,7 +148,6 @@ export default function Home() {
                                 Carregando...
                             </p>
                         )}
-                        <AdBanner />
                     </>
                 )}
             </main>
