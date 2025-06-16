1844bed0bda0c72b0f732bd615aa6c75ccc5b8ad
alberto.lopes <cast.aberto@pe.sebrae.com.br>
2025-06-16 16:47:41 -0300

    Correção ads

--- src/pages/AdBanner.jsx
+++ src/pages/AdBanner.jsx
@@ -2,18 +2,14 @@ import React from "react";
 
 export default function AdBanner() {
     return (
-        <div
-            id="frame"
-            style={{ width: "300px", height: "auto" }}
-            className="mx-auto my-6"
-        >
+        <div className="flex flex-col items-center my-6">
             <iframe
                 data-aa="2398944"
-                src="https://ad.a-ads.com/2398944?size=300x250"
+                src="//ad.a-ads.com/2398944?size=300x250"
                 style={{
                     width: "300px",
                     height: "250px",
-                    border: "0",
+                    border: "0px",
                     padding: "0",
                     overflow: "hidden",
                     backgroundColor: "transparent",
@@ -24,8 +20,7 @@ export default function AdBanner() {
                 sandbox="allow-scripts allow-same-origin allow-popups"
             ></iframe>
             <a
-                id="preview-link"
-                style={{ display: "block", textAlign: "right", fontSize: "12px" }}
+                className="text-xs text-gray-500 mt-1"
                 href="https://aads.com/campaigns/new/?source_id=2398944&source_type=ad_unit&partner=2398944"
                 target="_blank"
                 rel="noopener noreferrer"
