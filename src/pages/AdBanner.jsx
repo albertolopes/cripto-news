a62a7b36a80e61f3c2d941285069b9b2be9e0833
alberto.lopes <cast.aberto@pe.sebrae.com.br>
2025-06-16 17:45:33 -0300

    update

--- src/pages/AdBanner.jsx
+++ src/pages/AdBanner.jsx
@@ -2,21 +2,7 @@ import React from "react";
 
 export default function AdBanner() {
     const html = `
-        <div style="width: 300px;">
-            <iframe 
-                data-aa='2398944' 
-                src='//ad.a-ads.com/2398944?size=300x250' 
-                style='width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'>
-            </iframe>
-            <a 
-                style="display: block; text-align: right; font-size: 12px" 
-                href="https://aads.com/campaigns/new/?source_id=2398944&source_type=ad_unit&partner=2398944"
-                target="_blank" 
-                rel="noopener noreferrer"
-            >
-                Advertise here
-            </a>
-        </div>
+        <div id="frame" style="width: 728px;"><iframe data-aa='2398959' src='//ad.a-ads.com/2398959?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style="display: block; text-align: right; font-size: 12px" id="frame-link" href="https://aads.com/campaigns/new/?source_id=2398959&source_type=ad_unit&partner=2398959">Advertise here</a></div>
     `;
 
     return (
