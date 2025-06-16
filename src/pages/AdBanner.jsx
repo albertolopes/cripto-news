7db0b5959628233069350ffad0e37cd6168a83dd
alberto.lopes <cast.aberto@pe.sebrae.com.br>
2025-06-16 17:01:26 -0300

    banner

--- src/pages/AdBanner.jsx
+++ src/pages/AdBanner.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 
 export default function AdBanner() {
-    return (
-        <div className="flex flex-col items-center my-6">
-            <iframe
-                data-aa="2398944"
-                src="//ad.a-ads.com/2398944?size=300x250"
-                style={{
-                    width: "300px",
-                    height: "250px",
-                    border: "0px",
-                    padding: "0",
-                    overflow: "hidden",
-                    backgroundColor: "transparent",
-                }}
-                title="a-ads"
-                scrolling="no"
-                frameBorder="0"
-                sandbox="allow-scripts allow-same-origin allow-popups"
-            ></iframe>
-            <a
-                className="text-xs text-gray-500 mt-1"
+    const html = `
+        <div id="frame" style="width: 300px;">
+            <iframe 
+                data-aa='2398944' 
+                src='//ad.a-ads.com/2398944?size=300x250' 
+                style='width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'>
+            </iframe>
+            <a 
+                style="display: block; text-align: right; font-size: 12px" 
+                id="frame-link" 
                 href="https://aads.com/campaigns/new/?source_id=2398944&source_type=ad_unit&partner=2398944"
-                target="_blank"
+                target="_blank" 
                 rel="noopener noreferrer"
             >
                 Advertise here
             </a>
         </div>
+    `;
+
+    return (
+        <div
+            className="flex justify-center my-6"
+            dangerouslySetInnerHTML={{ __html: html }}
+        />
     );
 }
