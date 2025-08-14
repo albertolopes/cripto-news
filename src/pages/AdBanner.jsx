10ce74f9b5189ac249401c84e43290d2ad450d2e
alberto.lopes <cast.aberto@pe.sebrae.com.br>
2025-08-14 18:47:56 -0300

    Resolvendo problemas de segurança de conteúdo misto — Forçar HTTPS para todos os recursos externos — Adicionando Política de Segurança de Conteúdo — Atualizando cabeçalhos de segurança

--- src/pages/AdBanner.jsx
+++ src/pages/AdBanner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AdBanner() {
     const html = `
-        <div id="frame" style="width: 300px;"><iframe data-aa='2398959' src='//ad.a-ads.com/2398959?size=300x250' style='width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style="display: block; text-align: right; font-size: 12px" id="frame-link" href="https://aads.com/campaigns/new/?source_id=2398959&source_type=ad_unit&partner=2398959">Advertise here</a></div>
+        <div id="frame" style="width: 300px;"><iframe data-aa='2398959' src='https://ad.a-ads.com/2398959?size=300x250' style='width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style="display: block; text-align: right; font-size: 12px" id="frame-link" href="https://aads.com/campaigns/new/?source_id=2398959&source_type=ad_unit&partner=2398959">Advertise here</a></div>
     `;
 
     return (
